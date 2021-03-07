cd94640b0d9933c2a88e6bd52312d5d87ce50732
moboqe <generalmaxis@yandex.ru>
2021-03-08 04:38:41 +0500

    upd

--- js/game.js
+++ js/game.js
@@ -4,4 +4,4 @@ function flipCard() {
   this.classList.toggle('flip');
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard());
+cards.forEach(card => card.addEventListener('click', flipCard);
