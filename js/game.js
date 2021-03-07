f31703d53ac32327f908574878218786c8db450d
moboqe <generalmaxis@yandex.ru>
2021-03-08 03:04:26 +0500

    upd

---
+++ js/game.js
@@ -0,0 +1,7 @@
+const cards = document.querySelectorAll('.memory_card');
+
+function flipCard() {
+  this.classList.toggle('flip');
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
